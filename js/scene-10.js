c2f6d08b369187b54d7e391aa6167f390e1abd23
Sanjoy Rozario <sanjoyrozario27@gmail.com>
2026-08-16 04:40:47 +0600

    scene 10 done

--- js/scene-10.js
+++ js/scene-10.js
@@ -27,14 +27,14 @@ let etdsTypingSession = 0;
 
 
 function createEtdsTinNumber() {
-    let tin = "";
+    // let tin = "";
 
-    for (let index = 0; index < 12; index += 1) {
-        const minimumDigit = index === 0 ? 1 : 0;
-        tin += Math.floor(Math.random() * (10 - minimumDigit)) + minimumDigit;
-    }
+    // for (let index = 0; index < 12; index += 1) {
+    //     const minimumDigit = index === 0 ? 1 : 0;
+    //     tin += Math.floor(Math.random() * (10 - minimumDigit)) + minimumDigit;
+    // }
 
-    return tin;
+    return `60056****529`;
 }
 
 
